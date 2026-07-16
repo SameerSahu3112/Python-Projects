ddc6b7033ea706e69d7a5f58cf55dc60a1e314f2
Sameer Sahu <manojsahu.developer@gmail.com>
2026-07-16 15:44:54 +0530

    Completed product list and view products

--- Grocery Management.py
+++ Grocery Management.py
@@ -1,3 +1,4 @@
+cart = []
 def customer():
     print("Welcome Customer!")
     print("1. View Grocery Items")
@@ -6,6 +7,9 @@ def customer():
     print("4. Update Cart")
     print("5.Generate Bill")
     print("6.Back To Main Menu")
+    customer_choice = int(input("Enter Your Choice: "))
+    if customer_choice == 1:
+        view_grocery_items()
 
 def admin():
     print("Welcome Owner!")
@@ -15,6 +19,9 @@ def admin():
     print("4. View Grocery items")
     print("5. Change Price of Grocery items")
     print("6. Back To Main Menu")
+    admin_choice = int(input("Enter You Choice: "))
+    if admin_choice == 4:
+        view_grocery_items()
     
 def main_menu(user_pin=1234,attempt=3):
     print("###### Grocery Management System ######")
@@ -39,5 +46,96 @@ def main_menu(user_pin=1234,attempt=3):
         print("Invalid Choice! Try Again.")
         main_menu(user_pin, attempt)
  
+def view_grocery_items():
+    print("Grocery Items Available")
+    print("1.Grains & Flours")
+    print("2.Pulses")
+    print("3.Spices")
+    print("4.Cooking Essentials")
+    print("5.Dairy Products")
+    print("6.Beverages")
+    print("You Will See Item In Order Of Product ID, Product Name, Price, Stock")
+    choice_grocery = int(input("Enter Your Choice No.: "))
+    if choice_grocery == 1:
+       for product in products_Grains_Flour:
+           print(product)
+
+    elif choice_grocery == 2:
+        for product in products_pulses:
+            print(product)
+
+    elif choice_grocery == 3:
+       for product in product_spices:
+           print(product)
+        
+    elif choice_grocery == 4:
+       for product in product_cooking_essential:
+           print(product)
+
+    elif choice_grocery == 5:
+        for product in product_dairy:
+            print(product)
+
+    elif choice_grocery == 6:
+       for product in product_beverages:
+           print(product)
+
+    else:
+        print("Wrong Input")
+
+# [Product ID, Product Name, Price, Stock]
+
+products_Grains_Flour = [
+    [101, "Rice", 60, 25],
+    [102, "Wheat Flour", 30, 25],
+    [103, "Maida", 40, 25],
+    [104, "Sooji", 40, 25],
+    [105, "Besan", 40, 25],
+    [106, "Poha", 40, 25],
+]
+products_pulses = [
+    [201, "Toor Dal", 50, 35],
+    [202, "Moong Dal", 50, 35],
+    [203, "Udad Dal", 50, 35],
+    [204, "Chana Dal", 50, 35],
+    [205, "Masoor Dal", 50, 35],
+    [206, "Kabuli Chana", 50, 35],
+    [207, "Rajma", 50, 35],
+]
+product_spices = [
+    [301, "Elaichi", 20, 10],
+    [302, "Dal Chini", 20, 10],
+    [303, "Kali Mirch", 20, 10],
+    [304, "Jeera", 20, 10],
+    [305, "Dhaniya", 20, 10],
+    [306, "Turmeric", 20, 10],
+    [307, "Lal Mirch", 20, 10],
+    [308, "Garam Masala", 20, 10],
+    [309, "Saunf", 20, 10],
+]
+product_cooking_essential = [
+    [401, "Cooking Oil", 100, 35],
+    [402, "Ghee", 500, 25],
+    [403, "Salt", 20, 35],
+    [404, "Sugar", 20, 35],
+    [405, "Vinegar", 40, 35],
+    [406, "Soya Sauce", 40, 35],
+]
+product_dairy = [
+    [501, "Milk", 60, 50],
+    [502, "Cheese", 50, 50],
+    [503, "Butter", 70, 50],
+    [504, "Yogurt", 30, 50],
+    [505, "Curd", 25, 50],
+    [506, "Paneer", 100, 50],
+]
+product_beverages = [
+    [601, "Tea", 10, 25],
+    [602, "Coffee", 15, 25],
+    [603, "Juice", 15, 25],
+    [604, "Water", 10, 25],
+    [605, "Energy Drink", 50, 25],
+]
 
 
+main_menu(user_pin=1234,attempt=3)
