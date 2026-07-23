11f45ebbde2e09115ef45827ef57d5939a415489
Sameer Sahu <manojsahu.developer@gmail.com>
2026-07-23 11:26:19 +0530

    exceptional handling added

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,21 @@
 while True:
+    try:
+        a = int(input("Enter A Number: "))
+        b = int(input("Enter Another Number: "))
+        c = input("Enter The operation you want to perform (+, -, *, /) and 1 for exit:)")
 
-    a = int(input("Enter A Number: "))
-    b = int(input("Enter Another Number: "))
-    c = input("Enter The operation you want to perform (+, -, *, /) and 1 for exit:)")
+        if c == "+":
+            print(a+b)
+        elif c == "-":
+            print(a-b)
+        elif c == "*":
+            print(a*b)
+        elif c == "/":
+            print(a/b)
+        elif c == "1":
+            break
+        else:
+            print("Invalid operation")
 
-    if c == "+":
-        print(a+b)
-    elif c == "-":
-        print(a-b)
-    elif c == "*":
-        print(a*b)
-    elif c == "/":
-        print(a/b)
-    elif c == "1":
-        break
-    else:
-        print("Invalid operation")
+    except:
+        print("Please Enter Number")
