2e06334ef88ff443e0b88aa8deb91caf19341d64
Sameer Sahu <manojsahu.developer@gmail.com>
2026-08-23 15:10:27 +0530

    first c program

--- Bank Management/Model.py
+++ Bank Management/Model.py
@@ -1,10 +1,27 @@
-class Employee:
-    def __init__(self, employee_id, name, position, salary):
-        self.employee_id = employee_id
-        self.name = name
-        self.position = position
-        self.salary = salary
+class Customer:
+    def __init__(self, customer_name, date_of_birth, email, phone, address):
+        from Connection_Bank import create_connection
+        db = create_connection()
+        mycursor = db.cursor()
+        query = "INSERT into customer (customer_name, date_of_birth, email, phone, address) VALUES (%s, %s, %s, %s, %s)"
+        mycursor.execute(query, (customer_name, date_of_birth, email, phone, address))
+        db.commit()
+        db.close()
 
-    def __str__(self):
-        return f"Employee ID: {self.employee_id}, Name: {self.name}, Position: {self.position}, Salary: {self.salary}"
+def get_info():
+    customer_name = input("Enter The Name: ")
+    date_of_birth = input("Enter Your Date Of Birth (YYYY-MM-DD): ")
+    email = input("Enter Your Email: ")
+    phone = input("Enter Your Phone No.: ")
+    if len(phone) != 10:
+        print("Enter Valid Number")
+    address = input("Enter Your Address: ")
+    customer_info = Customer(customer_name, date_of_birth, email, phone, address)
+
     
+
+
+
+
+
+
